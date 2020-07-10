e07c35b61203833aa91f6c922bd7f23f6d3d7bb7
unknown <cappy_bappy@yahoo.com>
2020-07-09 18:58:49 -0700

    Made Random food generator, hopefully it'll work

--- TappyBird/Assets/Scripts/FoodRandomizer.cs
+++ TappyBird/Assets/Scripts/FoodRandomizer.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FoodRandomizer : MonoBehaviour
 {
     [SerializeField]
-    public Sprite[] foods;
-    
+    public static Sprite[] foods;
+
+
+    static Sprite FoodRandom() {
+        System.Random randoGenerate = new System.Random();
+         return foods[randoGenerate.Next(0, 20)];
+    }
 }
