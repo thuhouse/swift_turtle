014bbb3dd2e33c9da3dbf08d165f98d88cd579ae
unknown <cappy_bappy@yahoo.com>
2020-07-09 17:14:30 -0700

    Argh I have to do this to install your changes dangit

--- TappyBird/Assets/Scripts/FoodRandomizer.cs
+++ TappyBird/Assets/Scripts/FoodRandomizer.cs
@@ -5,16 +5,6 @@ using UnityEngine;
 public class FoodRandomizer : MonoBehaviour
 {
     [SerializeField]
-    Sprite[] foods;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Sprite[] foods;
+    
 }
